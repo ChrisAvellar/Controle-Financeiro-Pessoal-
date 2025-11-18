9022432f8fdee9edd1081df508105fedb4e19b20
Christian <2kavellar@gmail.com>
2025-11-18 11:35:37 -0300

    esqueleto do projeto

---
+++ src/test/java/com/christian_avellar/ControleFinancasPessoais/ControleFinancasPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christian_avellar.ControleFinancasPessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinancasPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
